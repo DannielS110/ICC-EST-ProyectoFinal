c00564f2e4fa099799ac4210bd2a7386d1013feb
Daniel Sanchez <166523536+01010101010101011010010101101@users.noreply.github.com>
2025-07-31 18:16:36 -0500

    creacion recursivo2D y Recursivo 4 D/ resultadoEjecucion

--- proyectoFinal/src/model/ResultadoEjecucion.java
+++ proyectoFinal/src/model/ResultadoEjecucion.java
@@ -8,5 +8,125 @@ import java.util.ArrayList;
  * Incluye métricas de Programación Dinámica
  */
 public class ResultadoEjecucion {
+    private String nombreAlgoritmo;
+    private long tiempoEjecucion; // En milisegundos
+    private long tiempoEjecucionNs; // En nanosegundos
+    private int celdasVisitadas;
+    private List<Celda> camino;
+    private boolean encontroSolucion;
     
+    private boolean usoProgramacionDinamica;
+    private int cachesUtilizados;
+    private int tamanoCache;
+    
+    public ResultadoEjecucion(String nombreAlgoritmo) {
+        this.nombreAlgoritmo = nombreAlgoritmo;
+        this.camino = new ArrayList<>();
+        this.celdasVisitadas = 0;
+        this.encontroSolucion = false;
+        this.usoProgramacionDinamica = false;
+        this.cachesUtilizados = 0;
+        this.tamanoCache = 0;
+    }
+    
+    public String getNombreAlgoritmo() { 
+        return nombreAlgoritmo; 
+    }
+    
+    public long getTiempoEjecucion() { 
+        return tiempoEjecucion; 
+    }
+    
+    public void setTiempoEjecucion(long tiempoEjecucion) { 
+        this.tiempoEjecucion = tiempoEjecucion; 
+    }
+    
+    public long getTiempoEjecucionNs() { 
+        return tiempoEjecucionNs; 
+    }
+    
+    public void setTiempoEjecucionNs(long tiempoEjecucionNs) { 
+        this.tiempoEjecucionNs = tiempoEjecucionNs;
+        // Actualizar también el tiempo en milisegundos
+        this.tiempoEjecucion = tiempoEjecucionNs / 1_000_000;
+    }
+    
+    public int getCeldasVisitadas() { 
+        return celdasVisitadas; 
+    }
+    
+    public void setCeldasVisitadas(int celdasVisitadas) { 
+        this.celdasVisitadas = celdasVisitadas; 
+    }
+    
+    public List<Celda> getCamino() { 
+        return camino; 
+    }
+    
+    public void setCamino(List<Celda> camino) { 
+        this.camino = camino; 
+    }
+    
+    public boolean isEncontroSolucion() { 
+        return encontroSolucion; 
+    }
+    
+    public void setEncontroSolucion(boolean encontroSolucion) { 
+        this.encontroSolucion = encontroSolucion; 
+    }
+    
+    public int getLongitudCamino() { 
+        return camino.size(); 
+    }
+    
+    //para Programación Dinámica
+    public boolean isUsoProgramacionDinamica() { 
+        return usoProgramacionDinamica; 
+    }
+    
+    public void setUsoProgramacionDinamica(boolean usoProgramacionDinamica) { 
+        this.usoProgramacionDinamica = usoProgramacionDinamica; 
+    }
+    
+    public int getCachesUtilizados() { 
+        return cachesUtilizados; 
+    }
+    
+    public void setCachesUtilizados(int cachesUtilizados) { 
+        this.cachesUtilizados = cachesUtilizados; 
+    }
+    
+    public void incrementarCachesUtilizados() {
+        this.cachesUtilizados++;
+    }
+    
+    public int getTamanoCache() { 
+        return tamanoCache; 
+    }
+    
+    public void setTamanoCache(int tamanoCache) { 
+        this.tamanoCache = tamanoCache; 
+    }
+    
+    /**
+     * Genera un resumen de las métricas para debugging
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ResultadoEjecucion{");
+        sb.append("algoritmo='").append(nombreAlgoritmo).append('\'');
+        sb.append(", tiempoMs=").append(tiempoEjecucion);
+        sb.append(", tiempoNs=").append(tiempoEjecucionNs);
+        sb.append(", celdasVisitadas=").append(celdasVisitadas);
+        sb.append(", longitudCamino=").append(getLongitudCamino());
+        sb.append(", encontroSolucion=").append(encontroSolucion);
+        if (usoProgramacionDinamica) {
+            sb.append(", usoPD=true");
+            sb.append(", cachesUsados=").append(cachesUtilizados);
+            sb.append(", tamanoCache=").append(tamanoCache);
+        }
+        sb.append('}');
+        return sb.toString();
+    }
 }
